86ee14cbcba8627cc93ce953fce439e9e397a917
albatechnology <70558798+albatechnology@users.noreply.github.com>
2022-10-13 12:02:39 +0700

    [REMOVE] LaravelPdoOdbc\ODBCServiceProvider::class

--- packages/sms-backend/app/Http/Requests/API/V1/CustomerDeposit/UpdateActivityRequest.php
+++ packages/sms-backend/app/Http/Requests/API/V1/CustomerDeposit/UpdateActivityRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\API\V1\Activity;
+namespace App\Http\Requests\API\V1\CustomerDeposit;
 
 use App\Classes\DocGenerator\BaseApiRequest;
 use App\Classes\DocGenerator\Interfaces\ApiDataExample;
